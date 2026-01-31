006efbb07f627635984140dcdb71e058ab680e4b
cliffchen <t-cliffchen+github@microsoft.com>
2026-01-31 06:31:10 +0000

    chore: update test script
    Signed-off-by: cliffchen <t-cliffchen+github@microsoft.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,22 @@
-    # -c platform_tests/link_flap/test_link_flap.py \
+#!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 <test_case>"
+    echo "Example: $0 console/test_console_monitor.py"
+    exit 1
+fi
 
-cd /home/cliffchen/sonic-mgmt-int/tests
+# Remove "tests/" prefix from the test case path if present
+TEST_CASE=$(echo "$1" | sed 's|^tests/||' | sed 's|^\./tests/||')
 
-./run_tests.sh \
-    -c console/test_get_nb.py \
+docker exec --user cliffchen -ti sonic-mgmt-cliffchen bash -c \
+    "cd /home/cliffchen/sonic-mgmt-int/tests && ./run_tests.sh \
+    -c $TEST_CASE \
     -a False \
     -e --skip_sanity \
     -e --disable_loganalyzer \
-    -e "--neighbor_type=sonic" \
+    -e '--neighbor_type=sonic' \
+    -e '--disable_memory_utilization' \
     -f ../ansible/testbed.yaml \
     -i ../ansible/bjw3,../ansible/veos \
     -m individual \
@@ -16,38 +24,4 @@ cd /home/cliffchen/sonic-mgmt-int/tests
     -O \
     -r \
     -t any,m0,mx,m1,t0,t1 \
-    -u
-
-# ./run_tests.sh \
-#     -c telemetry/test_telemetry_poll.py \
-#     -a False \
-#     -e --skip_sanity \
-#     -e --disable_loganalyzer \
-#     -f ../ansible/vtestbed.yaml \
-#     -i ../ansible/bjw2,../ansible/veos \
-#     -m individual \
-#     -n testbed-vms-kvm-t1-lag \
-#     -O \
-#     -r \
-#     -t any,m0,mx,m1,t0,t1 \
-#     -u
-
-# ./run_tests.sh \
-#     -c bgp/test_bgp_fact.py \
-#     -e --skip_sanity \
-#     -e --disable_loganalyzer \
-#     -f ../ansible/testbed.yaml \
-#     -i ../ansible/bjw,../ansible/veos \
-#     -m individual \
-#     -n <testbed-name> \
-#     -o \
-#     -O \
-#     -t any,mx \
-#     -u
-
-
-# smoke test debug
-# python3 -m pytest test_pretest.py      --inventory=../ansible/veos,../ansible/bjw2 --host-pattern=all --testbed=testbed-bjw2-can-t1-8102-5     --testbed_file=../ansible/testbed.yaml --log-cli-level=warning --log-file-level=debug     --kube_master=unset --showlocals --assert=plain --show-capture=no -rav     --ignore=ptftests --ignore=acstests --ignore=saitests --ignore=scripts --ignore=k8s --ignore=sai_qualify      --log-file='logs/test_pretest|||testbed-bjw2-can-t1-8102-5.log' --junit-xml='logs/test_pretest|||testbed-bjw2-can-t1-8102-5.xml'  --topology t1,any --py_saithrift_url=http://10.150.22.222/pipelines/Networking-acs-buildimage-Official/broadcom/internal/latest/target/debs/bookworm/python-saithrift_0.9.4_amd64.deb --deep_clean --sad_case_list=sad_bgp,sad_lag_member,sad_lag,sad_vlan_port,sad_inboot --allow_recover
-
-# bjw-can-7250-1 test
-# python3 -m pytest test_pretest.py      --inventory=../ansible/veos,../ansible/bjw --host-pattern=all --testbed=bjw-can-7250-1     --testbed_file=../ansible/testbed.yaml --log-cli-level=warning --log-file-level=debug     --kube_master=unset --showlocals --assert=plain --show-capture=no -rav     --ignore=ptftests --ignore=acstests --ignore=saitests --ignore=scripts --ignore=k8s --ignore=sai_qualify      --log-file='logs/test_pretest|||bjw-can-7250-1.log' --junit-xml='logs/test_pretest|||bjw-can-7250-1.xml'  --topology t1,any --py_saithrift_url=http://10.150.22.222/pipelines/Networking-acs-buildimage-Official/broadcom/internal/latest/target/debs/bookworm/python-saithrift_0.9.4_amd64.deb --deep_clean --sad_case_list=sad_bgp,sad_lag_member,sad_lag,sad_vlan_port,sad_inboot --allow_recover
+    -u"
